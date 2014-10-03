f37ed45aac12d896b24daab508b4991d0d0662f0
Ceyhun Hallac <ceyhun.hallac@4finance.com>
2014-10-03 11:06:38 +0200

    First draft

---
+++ src/main/groovy/com/ofg/microservice/fraud/FraudController.groovy
@@ -0,0 +1,14 @@
+package com.ofg.microservice.fraud
+
+import groovy.util.logging.Slf4j
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@RestController
+@RequestMapping('/api')
+//@Api(value = "loanApplicationId", description = "")
+class FraudController {
+
+    
+}
---
+++ src/main/groovy/com/ofg/microservice/fraud/FraudService.groovy
@@ -0,0 +1,7 @@
+package com.ofg.microservice.fraud
+
+/**
+ * Created by Ceyhun.Hallac on 03/10/2014.
+ */
+class FraudService {
+}
