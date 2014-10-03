18647106b19dd59d3b9a2fb800e5b43336206c31
Ceyhun Hallac <ceyhun.hallac@4finance.com>
2014-10-03 13:49:30 +0200

    FraudServicce - new methods added

--- src/main/groovy/com/ofg/microservice/fraud/FraudService.groovy
+++ src/main/groovy/com/ofg/microservice/fraud/FraudService.groovy
@@ -37,18 +37,26 @@ class FraudService {
     boolean isAmountGreaterThanSecondThreshold(LoanApplication loanApplication) {
         return loanApplication.amount > SECOND_THRESHOLD
     }
+
+    boolean isAmountBetweenThresholds(LoanApplication loanApplication) {
+        return loanApplication.amount > FIRST_THRESHOLD && loanApplication.amount < SECOND_THRESHOLD
+    }
     
     boolean isClientNameTooShort(LoanApplication loanApplication) {
         return loanApplication.firstName.length() < 2 ||
                 loanApplication.lastName() < 2
     }
+
+    boolean isClientNameTooLong(LoanApplication loanApplication) {
+        return loanApplication.firstName.length() > 25 ||
+                loanApplication.lastName() < 25
+    }
     
     boolean isClientFishy(LoanApplication loanApplication) {
         return JobPosition.FINANCE_SECTOR.getName().equalsIgnoreCase(loanApplication.job) &&
                 loanApplication.age > MAX_AGE &&
-                loanApplication.amount > FIRST_THRESHOLD &&
-                loanApplication.amount < SECOND_THRESHOLD &&
-                isCustomerNameFinance(loanApplication.firstName, loanApplication.lastName)
+                isAmountBetweenThresholds(loanApplication) &&
+                isClientNameTooLong(loanApplication.firstName, loanApplication.lastName)
     }
 
     boolean isClientOK(LoanApplication loanApplication) {
