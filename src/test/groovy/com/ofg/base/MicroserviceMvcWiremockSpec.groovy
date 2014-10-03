7dec94d7a437621fe35302769afdfab9b3b1f930
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2014-10-03 08:19:48 +0200

    Removing tests ;)

--- src/test/groovy/com/ofg/base/MicroserviceMvcWiremockSpec.groovy
+++
@@ -1,9 +0,0 @@
-package com.ofg.base
-import com.ofg.infrastructure.base.MvcWiremockIntegrationSpec
-import com.ofg.microservice.Application
-import org.springframework.boot.test.SpringApplicationContextLoader
-import org.springframework.test.context.ContextConfiguration
-
-@ContextConfiguration(classes = [Application], loader = SpringApplicationContextLoader)
-class MicroserviceMvcWiremockSpec extends MvcWiremockIntegrationSpec {
-}
